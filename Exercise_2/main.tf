b9ac18e6b9968dfb1bec17f1e764321eec86dcbf
Anju <anjurose.d@gmail.com>
2020-02-17 10:59:59 +0000

    AWS Lambda with output variables and input variables

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region     = var.region
+}
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -21,19 +24,24 @@ EOF
 data "archive_file" "greet_lambda" {
   type        = "zip"
   source_file = "greet_lambda.py"
-  output_path = "outputs/greet_lambda.zip"
+  output_path = "var.lambda_zip_location"
 }
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "outputs/greet_lambda.zip"
+  filename      = "var.lambda_zip_location"
   function_name = "greet_lambda"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "greet_lambda.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-#  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+   #source_code_hash = "${base64sha256(file("var.lambda_zip_location"))}"
+ source_code_hash = "${filebase64sha256("var.lambda_zip_location")}"
 
   runtime = "python3.7"
 
+environment {
+    variables = {
+      greeting = "hello"
+    }
+  }
 }
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,11 @@
 # TODO: Define the variable for aws_region
+
+variable "region" {
+  default = "us-west-2"
+}
+
+
+variable "lambda_zip_location" {
+  type    = list(string)
+  default = ["outputs/greet_lambda.zip"]
+}
